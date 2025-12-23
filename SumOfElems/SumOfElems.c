7c15b316ea126a00c715f19990fa58a5e8f980a9
Alexander <pchp250g8@tutamail.com>
2025-12-23 12:05:46 +0300

    Добавьте файлы проекта.

---
+++ SumOfElems/SumOfElems.c
@@ -0,0 +1,45 @@
+﻿// SumOfElems.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+int main()
+{
+    int nElems;
+    int* iArray;
+    int nSum;
+    system("cls");
+    printf("Input a count of elements in array\r\n");
+    scanf_s("%d", &nElems);
+    iArray = calloc(nElems, sizeof(int));
+    if (iArray == NULL) return -1;
+    nSum = 0;
+    srand(time(NULL));
+    for (int i = 0; i < nElems; i++)
+    {
+        iArray[i] = 1 + rand() / 1000;
+    }
+    printf("Inputed array : ");
+    for (int i = 0; i < nElems; i++)
+    {
+        nSum += iArray[i];
+        printf("%d ", iArray[i]);
+    }
+    free(iArray);
+    printf("\r\nThe Sum of % d elements of the array is : % d\r\n",nElems, nSum);
+    _fgetchar();
+    fgetc(stdin);
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
